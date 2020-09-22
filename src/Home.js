1f71fc4be95edd9ba2f6e9673fa2d284def9b034
Zernalda Septian <zernaldaseptian@Zernaldas-MacBook-Air.local>
2020-09-22 19:28:03 +0700

    adding router

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,62 @@
-import React from 'react';
+// import React from 'react';
+// import Gallery from './components/Gallery';
+
+// const Home = ({handleLogout}) => {
+//     return (
+//         <section className="hero">
+//             <nav>
+//                 <h2>Welcome</h2>
+
+//                 <button onClick={handleLogout}>Logout</button>
+//             </nav>
+//                 <Gallery/>
+//         </section>
+//     );
+// };
+
+// export default Home;
+
+import React, { Component } from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 import Gallery from './components/Gallery';
 
-const Home = ({handleLogout}) => {
-    return (
-        <section className="hero">
-            <nav>
-                <h2>Welcome</h2>
-                <button onClick={handleLogout}>Logout</button>
-            </nav>
-                <Gallery/>
-        </section>
-    );
-};
+
+class Home extends Component {
+      
+      Gallery=()=> {
+        return <Gallery />
+      }
+      
+    render() {
+        const {handleLogout} = this.props
+        return (
+            <section className="hero">
+                <Router>
+                    <div>
+                        <nav className="nav-menu">
+                            <ul>
+                                <li className="test"><Link to="/">Home</Link></li>
+                                <li><Link to="/gallery">Gallery</Link></li>
+                                
+                            </ul>
+                            <button onClick={handleLogout}>Logout</button>
+                        </nav>
+                        <Switch>
+                            <Route exact path="/" />
+                            <Route path="/gallery">
+                                <Gallery />
+                            </Route>
+                        </Switch>
+                    </div>
+                </Router>
+            </section>
+        );
+    }
+}
 
 export default Home;
